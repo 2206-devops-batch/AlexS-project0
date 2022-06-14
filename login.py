afa22b65e058f1840ea070cc9ea5aca926485798
salyx21 <alex052@revature.net>
2022-06-14 15:08:46 -0400

    implemented sign up function and starting testing soon

--- login.py
+++ login.py
@@ -1,15 +1,22 @@
 from getpass import getpass
 import sys
 import csv
+import re
 
 username = ""
 password = ""
 
-def sign_up():
-    print("sign up")
+
+filename = 'client_info.csv'
+
+def sign_up(username, password):
+     with open(filename, 'a') as file:
+        file.write(f'{username}, {password}')
+
 
 def lookup_database():
-    print("searching in database...")
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
     return True
 
 def main():
@@ -18,8 +25,11 @@ def main():
 
         if (choice.lower() == "n"):
             choice1 = input("Would you like to sign up(Y/N)?: ")
-            if (choice1.lower() == "Y"):
-                sign_up()
+            if (choice1.lower() == "y"):
+                username = input("Username: ")
+                password = input("Password: ")
+                sign_up(username, password)
+                return False
             else:
                 print("sign in as guest")
         elif (choice.lower() == "y"):
