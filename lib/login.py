22b76bb9a6e6ce7aee0e879346255d1c75196e42
salyx21 <alex052@revature.net>
2022-06-16 21:11:54 -0400

    a lot of changes

--- lib/login.py
+++ lib/login.py
@@ -1,10 +1,11 @@
 from getpass import getpass
 from pickle import TRUE
+import os
 import sys
 import csv
 import re
 import time
-import pathlib
+from turtle import clear
 
 username = ""
 password = ""
@@ -12,19 +13,23 @@ password = ""
 filename = r'C:\Users\Chopp\Desktop\Revature\Projects\AlexS-project0\database\client_info.csv'
 
 def sign_up(username, password):
-    regex = re.match("^[a-zA-Z]\d+", username)
 
-    with open(filename, 'a') as file:
-        if regex != None:
-            file.write(f'{username}, {password}')
-            return True
-        else:
-            print("username must have letters and numbers")
-            return False
+    with open(filename, 'a+') as file:
+        file.writelines(f'{username},{password}\n')
+        print(f"Success! Welcome {username}")
 
+        time.sleep(1)
+        print("Reloading Screen soon")
 
-def loading_bar():
-    word = "Fetching Client Infomartion..."
+        time.sleep(2)
+        os.system('cls')
+    
+
+    main()
+        
+
+def loading_bar(word):
+    #word = "Fetching Client Infomartion..."
     toolbar_width = len(word)
   
     for i in range(toolbar_width):
@@ -32,37 +37,43 @@ def loading_bar():
         # update the bar
         print(word[i], end = "", flush = True)
 
-def lookup_database(username):
-    with open(filename, 'r') as file:
-            print(file.read())
+    return True
 
-        
+def lookup_database(username, password):
+    people = []
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+
+        for clients in reader:
+            people.append(clients)  
+
+    return (True in set(map(lambda x: username in x and password in x, people)))
+
 
 def main():
     if len(sys.argv) < 2:
         choice = input("Are you an existing shopper(y/n)? or sign in as guest(ENTER) ")
 
         if (choice.lower() == "n"):
-            choice1 = input("Would you like to sign up(Y/N)?: ")
+            choice1 = input("Would you like to sign up(y/n)?: ")
             if (choice1.lower() == "y"):
-                username = input("Username (Must have atleast letters and numbers): ")
+                username = input("Username: ")
                 password = input("Password: ")
                 sign_up(username, password)
-                return False
             else:
-                print("sign in as guest")
+                return False
         elif (choice.lower() == "y"):
             username = input("Username: ")
             password = getpass("Password(echo off): ")
 
-            return (lookup_database(username))            
+            return (lookup_database(username, password))            
         else:
-            print("sign in as guest")
-            return True
+            return False
     
     else:
         username = sys.argv[1]
         password = input("Please typed in a password: ") if (len(sys.argv) == 2) else sys.argv[2]    
-        lookup_database()
+        lookup_database(username, password)
+
 
 if __name__ == "__main__": main()
--- main.py
+++ main.py
@@ -1,32 +1,67 @@
 from lib import login
 import csv
+import os
+import time
+import math
 
-check = login.main()
-filename = "items.csv"
+# check = login.main()
+filename = r"C:\Users\Chopp\Desktop\Revature\Projects\AlexS-project0\database\items.csv"
 items = []
 choice2 = ""
-
-# def check():
-#     return login.main()
+cart = {}
+sum = []
 
 def menu(datalist):
     string= ""
-    for index, x in enumerate(datalist, start=1):
-        string += f'{index}) {x[0]} '
-        if ((index % 3) == 0 ):
-            string += '\n'
+    for x in datalist:
+        string += f'{x[0]} ${x[1]}\n'
     
     return string
 
+def lookup_item(item_name):
+    for x in items:
+        if item_name in x:
+            cart[x[0]] = x[1]
+            return True
+        
+    return False
+    
+
 def main():
-    if (check == True):
+    if (login.main() == True):
         with open(filename, 'r') as file:
             reader = csv.reader(file)
 
             for stuff in reader:
                 items.append(stuff)
-        
-            choice2 = input(menu(items) + "\nPlease select your item: ")
 
+        os.system('cls')
+        time.sleep(0.4)
+        login.loading_bar("Loading Items...")
+
+        time.sleep(0.8)
+        rerun = True
+        while (rerun):
+            os.system('cls')        
+            print(menu(items))
+
+            item_choice = input(f"Cart[{len(cart)}]\nPlease select you item(quit choosing by entering no input): ")
+
+            lookup_item(item_choice)
+            rerun = True if item_choice != '' else False
+        
+
+        os.system('cls')
+        print("Reciept: ")
+        for x, y in cart.items():
+            sum.append(y)
+            print(f'{x}.....${y}')
+        
+        print(f'\nTotal.....${math.fsum(list(map(float,sum)))}')
+        # print(math.fsum(list(map(float,cart))))
+        
+        
+    else:
+        print("No Username/Password input")
 
 if __name__ == "__main__": main()
